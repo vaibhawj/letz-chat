0f5c5ea1a3ff1356dc9524738bf5d67af628c948
Vaibhaw Jaiswal <vj.vaibhaw@gmail.com>
2018-09-14 22:42:17 +0530

    Change to org repo url

--- client/app.js
+++ client/app.js
@@ -15,7 +15,7 @@ class App extends React.Component {
             <Route component={Home} />
           </Switch>
           <div className="github">
-            <a href="https://github.com/vaibhawj/ws-chat" target="_blank"
+            <a href="https://github.com/WTFun/ws-chat" target="_blank"
               rel="noopener noreferrer">
               <img src="/github.png" className="githubImg" alt="github" />
             </a>
