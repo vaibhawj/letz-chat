5870797ec4b3824b2563d1cb7605428f80c9dbf4
Vaibhawk <vaibhawk@in-vaibhawk.local>
2018-09-18 17:44:19 +0530

    revert pings

--- server.js
+++ server.js
@@ -53,10 +53,6 @@ app.ws.use(route.all('/chat/:roomName', function (ctx, roomName) {
     sendPopulationStat();
   });
 
-  setInterval(() => {
-    ctx.websocket && ctx.websocket.send(`{"ping": ${new Date().toTimeString()}}`);
-  }, 1000);
-
 }));
 
 app.use(function* index() {
